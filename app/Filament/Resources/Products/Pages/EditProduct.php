112b15e4c0e3f16e852aacdf163b014be120b3e0
IsraaSaede <ea08477@gmail.com>
2026-08-22 16:42:57 +0300

    fix product image url

--- app/Filament/Resources/Products/Pages/EditProduct.php
+++ app/Filament/Resources/Products/Pages/EditProduct.php
@@ -9,20 +9,33 @@ use Filament\Resources\Pages\EditRecord;
 class EditProduct extends EditRecord
 {
     protected static string $resource = ProductResource::class;
+
     protected static ?string $title = 'تعديل المنتج';
 
     protected function getHeaderActions(): array
     {
         return [
+            \Filament\Actions\CreateAction::make()
+                ->label('إضافة منتج')
+                ->icon('heroicon-o-plus')
+                ->url(ProductResource::getUrl('create')),
 
-        \Filament\Actions\CreateAction::make()
-            ->label('إضافة منتج')
-            ->icon('heroicon-o-plus')
-            ->url(ProductResource::getUrl('create')),
             DeleteAction::make()
                 ->label('حذف المنتج')
                 ->icon('heroicon-o-trash')
                 ->requiresConfirmation(),
         ];
+    }
+
+    /**
+     * إذا لم يتم رفع صورة جديدة، لا نغيّر الصورة القديمة.
+     */
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        if (empty($data['image'])) {
+            unset($data['image']);
+        }
+
+        return $data;
     }
 }
