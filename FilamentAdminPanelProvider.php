dd40c17af0fadc3b1ba0b5f559c390f03ee10159
IsraaSaede <ea08477@gmail.com>
2026-08-23 11:06:10 +0300

    Rename widget file to match class name (PSR-4 compliance)

---
+++ FilamentAdminPanelProvider.php
@@ -0,0 +1,3 @@
+[33m83331cf[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m Fix duplicate widget registration causing class redeclaration
+[33mbe7d1c2[m Fix duplicate low stock class
+[33m146ff19[m Fix duplicate low stock class
