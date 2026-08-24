a0fc87e2c20b10bbeeed6b2f1a9d81a5aaee55ee
IsraaSaede <ea08477@gmail.com>
2026-08-25 00:22:53 +0300

    six debu

--- app/Filament/Resources/Orders/Pages/ListOrders.php
+++ app/Filament/Resources/Orders/Pages/ListOrders.php
@@ -4,7 +4,6 @@ namespace App\Filament\Resources\Orders\Pages;
 
 use App\Filament\Resources\Orders\OrderResource;
 use App\Filament\Widgets\OrderStatsOverview;
-use App\Filament\Widgets\BestSellingProducts;
 use App\Filament\Widgets\TopAreasOrders;
 use App\Filament\Widgets\OrderSalesChart;
 use Filament\Resources\Pages\ListRecords;
@@ -28,7 +27,6 @@ class ListOrders extends ListRecords
     protected function getFooterWidgets(): array
     {
         return [
-            BestSellingProducts::class,
             TopAreasOrders::class,
             OrderSalesChart::class,
         ];
